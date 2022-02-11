c17a350267e1ab86abd5d8631d05184b7623ad33
luznaszelkach <jakubbrandys99@gmail.com>
2022-02-11 11:22:32 +0100

    Required

---
+++ 12174 Jakub Brandys termin 1/Controllers/OcenyController1.cs
@@ -0,0 +1,37 @@
+﻿using _12174_Jakub_Brandys_termin_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12174_Jakub_Brandys_termin_1.Controllers
+{
+
+    public class OcenyController1 : ControllerBase
+    {
+
+        private List<Oceny> ListaOceny;
+
+        public OcenyController1()
+        {
+            ListaOceny = new List<Oceny>()
+            {
+                new Oceny() {Album=123456,Punkty=22,Ocena="F"},
+                new Oceny() {Album=122356,Punkty=90,Ocena="A"},
+                new Oceny() {Album=123256,Punkty=70,Ocena="C"}
+
+            };
+        }
+
+        [Route("/api/exams")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Oceny>> GetCard()
+        {
+
+            return ListaOceny.ToList();
+        }
+
+    }
+}
+
+
+
---
+++ 12174 Jakub Brandys termin 1/Models/Oceny.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _12174_Jakub_Brandys_termin_1.Models
+{
+    public class Oceny
+    {
+        [Required]
+        [RegularExpression("^[0 - 9]{6, 6}$")]
+        public int Album { get; set; }
+        [Required]
+        [Range (0, 100)]
+        public int Punkty { get; set; }
+        public string Ocena { get; set; }
+
+        public DataType Data { get; set; }
+    }
+}
