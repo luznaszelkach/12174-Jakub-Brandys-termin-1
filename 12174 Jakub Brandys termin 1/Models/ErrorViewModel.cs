f3aff26df6c01e80f31033cf4c761e05dc3e798b
luznaszelkach <jakubbrandys99@gmail.com>
2022-02-11 10:30:15 +0100

    Add project files.

---
+++ 12174 Jakub Brandys termin 1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _12174_Jakub_Brandys_termin_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
